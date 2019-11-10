947eb6b5a0022405fec440db4629e6d675073338
mariiabondarenko <mashabond20@gmail.com>
2019-11-10 22:10:12 +0200

    admin dashboard

--- CakeShop/App_Start/BundleConfig.cs
+++ CakeShop/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace CakeShop
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Content/bootstrap.js"));
+
             // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
             // готово к выпуску, используйте средство сборки по адресу https://modernizr.com, чтобы выбрать только необходимые тесты.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
