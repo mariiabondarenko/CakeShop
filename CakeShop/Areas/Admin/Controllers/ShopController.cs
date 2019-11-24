4a4bcbcf31a9e1a74a0603fbfa7a6a1478ce23ec
mariiabondarenko <mashabond20@gmail.com>
2019-11-24 19:04:00 +0200

    added categories controllers

---
+++ CakeShop/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,131 @@
+﻿using CakeShop.Models.Data;
+using CakeShop.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CakeShop.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
+        {
+            //Declare list of model
+            List<CategoryVM> categoryVMlist;
+            
+            using (Db db = new Db())
+            {
+                //Init the list
+                categoryVMlist = db.Categories
+                                    .ToArray()
+                                    .OrderBy(x => x.Sorting)
+                                    .Select(x => new CategoryVM(x))
+                                    .ToList();
+            }
+
+
+                //Retirn the view with the list
+                return View(categoryVMlist);
+        }
+
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            string id;
+
+            using (Db db = new Db())
+            {
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "titletaken";
+
+                CategoryDTO dto = new CategoryDTO();
+
+               dto.Name = catName;
+               dto.Slug = catName.Replace(" ", "-").ToLower();
+               dto.Sorting = 100;
+
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                id = dto.Id.ToString();
+
+            }
+
+            return id;
+                
+        }
+
+
+
+        //POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+
+            using (Db db = new Db())
+            {
+                //Set initial count
+                int count = 1;
+
+                //Declare DTO
+                CategoryDTO dto;
+
+                //Set sorting for each category
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+
+            }
+
+
+        }
+
+
+        //GET: Admin/Shop/DeleteCategory/id
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                CategoryDTO dto = db.Categories.Find(id);
+
+                db.Categories.Remove(dto);
+
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Categories");
+        }
+
+        //POST: Admin/Shop/RenameCategory
+        [HttpPost]
+        public string RenameCategory(string newCatName, int id)
+        {
+            using (Db db = new Db())
+            {
+                if (db.Categories.Any(x => x.Name == newCatName))
+                    return "titletaken";
+
+                CategoryDTO dto = db.Categories.Find(id);
+
+                dto.Name = newCatName;
+                dto.Slug = newCatName.Replace(" ", "-").ToLower();
+
+                db.SaveChanges();
+
+            }
+
+            return "ok";
+
+        }
+    }
+}
--- CakeShop/Models/Data/Db.cs
+++ CakeShop/Models/Data/Db.cs
@@ -8,8 +8,8 @@ namespace CakeShop.Models.Data
 {
     public class Db : DbContext
     {
-
         public DbSet<PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
+        public DbSet<CategoryDTO> Categories { get; set; }
     }
 }
